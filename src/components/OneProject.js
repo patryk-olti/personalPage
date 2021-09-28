b91a3d30351680d7b1f3bec877bcd92bbd9c5cf6
Patryk Oltuch <95oltuchpatryk@gmail.com>
2021-09-28 20:25:32 +0200

    rebuild project section

--- src/components/OneProject.js
+++ src/components/OneProject.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+
+import { AppContext } from '../AppContext';
 
 import '../styles/OneProject.css';
 
-const OneProject = ({ img }) => {
+const OneProject = ({ dbItem }) => {
+
+    const [ isActive, setIsActive ] = useState(false);
+
+    const styles = {
+        isActiveStyle: {
+            display: 'block'
+        },
+        isDisactiveStyle: {
+            display: 'none'
+        }
+    }
+
+    const { isPolish } = useContext(AppContext);
+
 
     return(
-            <div className='oneProject__container'>
-                <div className='oneProject__hoverDiv'>
-                    <span> Tekst </span>
+            <div className='oneProject__container' 
+                onMouseEnter={() => setIsActive(true)} 
+                onMouseLeave={() => setIsActive(false)}
+            >
+
+                <div style={ isActive ? styles.isActiveStyle : styles.isDisactiveStyle }>
+                    <div className="oneProject__content">
+                        <div><span className="oneProject__title"> { isPolish ? dbItem.titlePL : dbItem.titleENG} </span></div>
+                        <div><span className="oneProject__desc"> {dbItem.description} </span></div>
+                        <div><a href={dbItem.ghPages} > <button><span><FontAwesomeIcon icon={faGlobe} /></span> live </button></a> </div>
+                    </div>
                 </div>
 
+
                 <div className='oneProject__imgDiv'>
-                    <img src={img} alt='img'></img>
+                    <img src={dbItem.imgLink} alt='img'></img>
                 </div>
+
             </div>
     )
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 import OneProject from "../components/OneProject";
+
 import imgSyskon from '../img/projects/syskon.png';
 import imgPizza from '../img/projects/pizza.png';
+import imgAcademy from '../img/projects/akademia.png';
 
 import '../styles/Projects.css';
 
@@ -13,6 +15,7 @@ const Projects = () => {
             id: 1,
             titlePL: "strona firmy SysKon",
             titleENG: "SysKon's website",
+            description: "HTML5, CSS3, JS",
             ghPages: "https://patryk-olti.github.io/syskon/#",
             ghRepo: "https://github.com/patryk-olti/personalPage",
             imgLink: imgSyskon
@@ -21,6 +24,7 @@ const Projects = () => {
             id: 2,
             titlePL: "strona pizzerii",
             titleENG: "pizza place website",
+            description: "HTML5, CSS3, JS",
             ghPages: "https://patryk-olti.github.io/syskon/#",
             ghRepo: "https://github.com/patryk-olti/PizzaPlace",
             imgLink: imgPizza
@@ -28,19 +32,24 @@ const Projects = () => {
         ,
         {
             id: 3,
-            titlePL: "empty",
-            titleENG: "SysKon's website",
-            ghPages: "https://patryk-olti.github.io/syskon/#",
+            titlePL: "centrum dziecka",
+            titleENG: "children's academy",
+            description: "HTML5, CSS3, JS, React, JSX",
+            ghPages: "https://lukas960.github.io/CentrumDziecka/?fbclid=IwAR1Kee99ENBC-cr2j4AzKC36FvFckeOoF7eod5FmtoIEbb5jfUsdgeFCqSY",
             ghRepo: "https://github.com/patryk-olti/personalPage",
-            imgLink: imgSyskon
+            imgLink: imgAcademy
         },
     ]
 
     return(
         <div className="projects__container" id="projects">
             <div className="projects__content">
-                <OneProject img={listOfProjects[0].imgLink} />
+                <OneProject dbItem={listOfProjects[0]} />
+                <OneProject dbItem={listOfProjects[1]} />
+                <OneProject dbItem={listOfProjects[2]} />
             </div>
+
+            <div className="projects__background">projects</div>
         </div>
     )
 }
